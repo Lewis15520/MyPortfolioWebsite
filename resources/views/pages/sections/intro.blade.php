7e214b22f31092e59869af1db48b87881bd6662e
Lewis Hayter <Lewis15520@users.noreply.github.com>
2021-06-05 13:43:46 +0100

    update

--- resources/views/pages/sections/intro.blade.php
+++ resources/views/pages/sections/intro.blade.php
@@ -9,6 +9,13 @@
             <div class="actions">
                 <a href="#" class="dftButton">Get In Touch</a>
             </div>
+            <div class="mobile-links">
+                <ul>
+                    <li><a href="https://github.com/Lewis15520" target="_blank"><i class="fab fa-github"></i></a></li>
+                    <li><a href="https://www.linkedin.com/in/lewishayter/" target="_blank"><i class="fab fa-linkedin-in"></i></a></li>
+                    <li><a href="https://www.instagram.com/lewisowainhayter" target="_blank"><i class="fab fa-instagram"></i></a></li>
+                </ul>
+            </div>
         </div>
     </div>
 </div>
